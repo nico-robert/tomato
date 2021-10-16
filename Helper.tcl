262e4f5ba8a4e77bf3b05b41fb1f81fe430ef1d8
nico-robert <nicolas.robert.19000@gmail.com>
2021-10-16 10:06:16 +0200

    Add tolerance geom and equal + tcl::mathfunc

--- Helper.tcl
+++ Helper.tcl
@@ -13,10 +13,27 @@ namespace eval tomato::helper {
     # actual number, where in a number as: 0.134556 * 10^5 the digits are 0.134556 and the exponent is 5.
     variable DoubleWidth 53
 
+    # default tolerances values
+    variable TolEquals 1e-8
+    variable TolGeom   1e-12
+
+}
+
+namespace eval tcl::mathfunc {
+    # Pi value
+    proc Pi {} {
+        return [expr {acos(-1)}]
+    }
+    # Opposite value
+    proc Inv x {
+        return [expr {$x * -1}]
+    }
 }
 
 proc tomato::helper::Pi {} {
     # Defines the value of Pi
+    #
+    # ***Obsolete*** use `[expr {Pi()}]` instead.
     return [expr {acos(-1)}]
 }
 
@@ -39,7 +56,7 @@ proc tomato::helper::DegreesToRadians {degrees} {
     #
     # Returns The angle expressed in radians.
 
-    set degToRad [expr {[tomato::helper::Pi] / 180.0}]
+    set degToRad [expr {Pi() / 180.0}]
     return [expr {$degrees * $degToRad}]
 }
 
@@ -50,7 +67,7 @@ proc tomato::helper::RadiansToDegrees {radians} {
     #
     # Returns The angle expressed in degrees.
 
-    set radToDeg [expr {180.0 / [tomato::helper::Pi]}]
+    set radToDeg [expr {180.0 / Pi()}]
     return [expr {$radians * $radToDeg}]
 }
 
@@ -115,6 +132,35 @@ proc tomato::helper::MachineEpsilon {} {
     }
 
     return $eps
+}
+
+proc tomato::helper::L2Norm {x y z {exception False}} {
+    # Check if the provided values are scaled to L2 norm
+    #
+    # x - The x component
+    # y - The y component
+    # z - The z component
+    # exception - Throw an exception if result is false
+    #
+    # Returns True or false 
+    set norm [expr {sqrt(($x**2) + ($y**2) + ($z**2))}]
+
+    if {$norm < $::tomato::helper::Epsilon} {
+        if {$exception} {
+            throw {LessNormEuclideanNorm} "The Euclidean norm of x, y, z is less than tolerance Epsilon" 
+        }
+        return 0
+    }
+
+    if {abs($norm - 1.0) > $::tomato::helper::Epsilon} {
+        if {$exception} {
+            throw {MoreNormEuclideanNorm} "The Euclidean norm of x, y, z differs more than tolerance Epsilon from 1" 
+        }
+        return 0
+    }
+
+    return 1
+
 }
 
 namespace eval tomato::helper {
